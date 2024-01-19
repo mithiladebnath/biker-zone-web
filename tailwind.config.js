8a3d18d9523aadd18a0148efc573841c34c7b9bb
Your Namemithiladebnath <mithiladebnath@gmail.com>
2024-01-19 15:09:36 -0800

    adding code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            clifford: '#da373d',
+
+        },
     },
     plugins
     +-*********: [],
